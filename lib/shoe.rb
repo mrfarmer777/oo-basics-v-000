a9340a1e0bcf0937750a64a44e40e222079b497e
Matt Farmer <mrfarmer777@gmail.com>
2018-05-28 20:35:50 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+def Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand=brand
+  end
+
+  def cobble
+    puts "Shoe has been repaired!"
+    @condition="new"
+  end
+end
+
+  
